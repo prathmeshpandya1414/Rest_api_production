0ca541d567bbb9ac5955b802bb596ab7fadb1806
unknown <prathmesh1414>
2024-12-22 16:01:13 +0530

    Created the first api APIView.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
+from django.db import models  
+from django.contrib.auth.models import AbstractBaseUser 
+from django.contrib.auth.models import PermissionsMixin 
 from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
