cd76331607f3bf423ab77aa7286d56b386ad6c4b
Prathmesh Pandya <prathmesh1414>
2024-12-23 13:00:12 +0530

    Added permissions and authentication to system.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,8 +4,12 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+
+
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -94,3 +98,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authenticatin_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
